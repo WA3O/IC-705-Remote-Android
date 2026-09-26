76fd994da5c0ae972a9b24a194d424c2a9c595f8
WA3O <151731430+WA3O@users.noreply.github.com>
2026-09-26 04:30:56 -0400

    Add APK build support: app/build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.ic_705remote2"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.ic_705remote2"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2840
+        versionName = "28.40"
+    }
+}
+
+dependencies {
+}
